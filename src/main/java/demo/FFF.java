db2ee1d6efd16325c0f92941be9e075f5e3185fc
hengyunabc <hengyunabc@gmail.com>
2020-09-18 19:12:20 +0800

    update

--- src/main/java/demo/FFF.java
+++ src/main/java/demo/FFF.java
@@ -92,8 +92,268 @@ public class FFF {
             postDetail.setUserId(basePostDTO.getAuthorId());
         }
         
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
         
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
         
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
+        {
+            BasePostDetailDTO basePostDTO = new BasePostDetailDTO();
+
+            PostDetailVO postDetail =  new PostDetailVO();
+            postDetail.setPostId(basePostDTO.getPostId());
+            postDetail.setGmtCreate(basePostDTO.getGmtCreate());
+            postDetail.setGmtCreateStr(DisplayTextUtils
+                    .calcPostTimeText(postDetail.getGmtCreate(), System.currentTimeMillis()));
+            postDetail.setStatus(basePostDTO.getStatus());
+            postDetail.setNameSpace(basePostDTO.getNamespace());
+            Object userId = null;
+            postDetail.setMySelf(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setQuality(MapUtils.getString(basePostDTO.getExtra(), "quality"));
+            postDetail.setCanDelete(Objects.equals(userId, basePostDTO.getAuthorId()));
+            postDetail.setUserId(basePostDTO.getAuthorId());
+        }
         
 	}
 
